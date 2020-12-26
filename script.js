d1c3977e6d2e309bfb297277c348ceaacf06ba71
Young Ji Kim <youjmik@gmail.com>
2020-12-26 14:56:54 -0500

    Updating HW

--- script.js
+++ script.js
@@ -11,24 +11,37 @@ setInterval(function () {
 }, 1000)
 
 //Set up Button Click function to call info
-$("#btn").on("click",function (e) {
+$("#btn").on("click", function (e) {
     e.preventDefault();
-    var ApiKey ="964e1a54e21fa546d4037b013310911c"
+    var ApiKey = "964e1a54e21fa546d4037b013310911c"
     var cityName = $("#enterCity").val().trim()
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName+ "&appid="+ ApiKey;
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + ApiKey;
 
 
     $.ajax({
         type: "GET",
         url: queryURL,
-    }) 
-    .then(function(response) {
-       console.log(response)
-    }) 
+    })
+        .then(function (response) {
+            console.log(response); {
+
+                //City to Pull with Date on Main
+                var cityMain = $("#cityName").text(response.city.name);
+                console.log(cityMain)
+   
+
+                //List of Cities pulled in Search Bar//
+                var searchCity =$("#input-city-append").text(response.city.name)
+                // var searchTemp =$("$input-temp-append").text(response.main.temp + '')
+                // console.log(searchTemp)
+
+                //Weather and Icons
+
+                // var wicon = 
+            }
+        })
 })
 
-//Date to pull and City to Pull
-// var dayDate = moment().format("MMM Do YY")
-// var seeCity = 
+
 
 
