92e500a4ad19285e518c001a663e77ed4b67da03
Young Ji Kim <youjmik@gmail.com>
2020-12-28 17:30:25 -0500

    shifted Temps to Main

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var dailyHour = moment().format('H')
 var currentDate = $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 var ApiKey = "964e1a54e21fa546d4037b013310911c"
-
 //Set Timer to go live. //
 setInterval(function () {
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
@@ -10,10 +9,11 @@ setInterval(function () {
 //Set up Button Click function to call info
 $("#btn").on("click", function (e) {
     e.preventDefault();
+    
     var cityName = $("#enterCity").val().trim()
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + ApiKey;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial"+"&appid=" + ApiKey;
     if (cityName === "") {
-        alert ("Please put City name. ")
+        alert ("Please put valid City name. ")
     }
 
     $.ajax({
@@ -24,16 +24,19 @@ $("#btn").on("click", function (e) {
         .then(function (response) {
             console.log(response); {
 
-                //City to Pull with Date on Main
+                //City, Temps to Pull on Main
                 var cityMain = $("#cityName").text(response.name);
+                var searchTemp = $("#input-temp-append").text("Temperature: " + response.main.temp + "°F")
+                var searchFeels = $("#input-feels-like").text("But Feels Like: " +response.main.feels_like + "°F")
 
-                //List of Cities pulled in Search Bar//
+                //List of Information pulled to Search/
                 var searchCity = $("#input-city-append").text(response.name)
-                var searchTemp = $("#$input-temp-append").text("Temperature:" + response.main.temp + "°F")
+               
+                
 
                 //Weather and Icons
                 var wiconURL = "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png"
-                // var wicon = 
+                var wicon = $("<img src='" + wiconURL+ "'>")
                 // var wicon = 
             }
 
