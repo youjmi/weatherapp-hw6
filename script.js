78a3cd869a9e77c1985638107350486b0c1529f1
Young Ji Kim <youjmik@gmail.com>
2020-12-30 13:09:05 -0500

    finalizing ls

--- script.js
+++ script.js
@@ -2,21 +2,27 @@
 var currentDate = $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 var ApiKey = "964e1a54e21fa546d4037b013310911c"
 
+
+
 //Set Timer to go live. //
 setInterval(function () {
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 }, 1000)
 
+
 //Set up Button Click function to call info
 $("#btn").on("click", function (e) {
     e.preventDefault();
 
+    
     var cityName = $("#enterCity").val().trim()
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial" + "&appid=" + ApiKey;
     if (cityName === "") {
         alert("Please put valid City name. ")
     }
-
+    localStorage.setItem("cities", cityName)
+    var savedcity = localStorage.getItem("cities")
+    
     $.ajax({
         type: "GET",
         url: queryURL,
@@ -27,7 +33,7 @@ $("#btn").on("click", function (e) {
                 clearBox()
 
                 //City, Temps to Pull on Main
-                var cityMain = $("<h2>").text("Today's weather in: "+ response.name);
+                var cityMain = $("<h2>").text("Today's weather in: " + response.name);
                 var searchTemp = $("<h5>").text("Temperature: " + response.main.temp + "°F")
                 var searchFeels = $("<h5>").text("But Feels Like: " + response.main.feels_like + "°F")
 
@@ -82,7 +88,7 @@ $("#btn").on("click", function (e) {
                     $("#MainInfo").append(uviVal)
 
                 })
-            
+
             var ForecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + ApiKey;
             $.ajax({
                 url: ForecastURL,
@@ -97,15 +103,15 @@ $("#btn").on("click", function (e) {
 
                     for (var i = 0; i < 6; i++) {
 
-                        
+
                         var date = moment().add(i, "days").format('l')
-                        var forDate = $("<h4>").text(date)
+                        var forDate = $("<h6>").text(date)
                         var tempHigh = $("<h6>").text("Highest Temp: " + forecast.daily[i].temp.max + "°F")
                         var tempLow = $("<h6>").text("Lowest Temp: " + forecast.daily[i].temp.min + "°F")
                         var forhumidity = $("<h6>").text("Humidity: " + forecast.daily[i].humidity + "%")
                         var wiconURL = $('<img src="https://openweathermap.org/img/wn/' + forecast.daily[i].weather[0].icon + '.png" alt="weather icon">')
 
-                        
+
                         $("#forecast").append(forDate, wiconURL, tempHigh, tempLow, forhumidity)
 
                     }
